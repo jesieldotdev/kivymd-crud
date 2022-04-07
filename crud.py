2003a33dd643f05af131282e31344853d0bc0e56
jesiel364 <jesiel364@gmail.com>
2022-04-07 03:38:08 -0400

    Adicionado SpeedDial

--- crud.py
+++ crud.py
@@ -1,6 +1,8 @@
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.list import TwoLineListItem
+from kivymd.uix.list import TwoLineListItem, TwoLineAvatarListItem
+from kivymd.uix.card import MDCardSwipe
+from kivy.properties import StringProperty
 from kivymd.toast import toast
 from kivy.lang import Builder
 from kivy.core.window import Window
@@ -9,21 +11,27 @@ from kivy.storage.jsonstore import JsonStore
 store = JsonStore('banco.json')
 
 # store.put('tito', nome = 'Mathieu', idade=32)
-
-
 Window.size = 300, 600
 kv = Builder.load_file('crud.kv')
+
+
+
+
+class ListComAvatar(TwoLineAvatarListItem):
+	source = StringProperty("data/logo/kivy-icon-256.png")
+
+
+
 
 
 
 class VendasWindow(MDBoxLayout):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
-
 		
 
 	def Adicionar(self):
-		nome = self.ids.nome.text
+		
 		idade = self.ids.idade.text
 		colecao = self.ids.colecao.text
 		
@@ -32,7 +40,7 @@ class VendasWindow(MDBoxLayout):
 			print(f'{colecao} já existe')
 			toast(f'{colecao} já existe')
 		else:
-			store.put(colecao, nome = nome, idade=idade)
+			store.put(colecao, nome = colecao, idade=idade)
 			toast(f'{colecao} foi adicionado')
 			
 
@@ -40,44 +48,62 @@ class VendasWindow(MDBoxLayout):
 		key = self.ids.colecao.text
 		if store.exists(key):
 			store.delete(key)
+			toast(f'{key} foi deletado')
+
 		else:
 			print(f'{key} não existe')
 			toast(f'{key} não existe')
+
 
 	def Filtrar(self):
 		print('Filtrar')
 		toast('Test Kivy Toast')
 
-	def Mostrar_db(self, **kwargs):
-		lb_nome = self.ids.lbl1
-		lb_idade = self.ids.lbl2
-		
-		res = store.find()
-		for key in store:
-			print(key)
-			lb_nome.text = str(store.get(key.nome))
-			lb_idade.text = str(item[1]["idade"])
 
-	def listar(self):
+	def Listar(self):
 		res = store.find()
 		for key in store:
 			nome = store.get(key)['nome']
 			idade = store.get(key)['idade']
 
-			self.ids.container.add_widget(TwoLineListItem(text = nome , secondary_text = idade))
-
+			self.ids.container.add_widget(ListComAvatar(text = f'[b]{nome}[/b]' , secondary_text = f'{idade} anos'))
+	
 		for key in store:
 			print(store.get(key)['nome'])
 			print(store.get(key)['idade'])
-
 
 			
 
 
 class MyCrud(MDApp):
+	data = {
+	'Mostrar todos': 'text-box-search',
+	'Adicionar': 'notebook-plus-outline', 
+	'Remover': 'delete', 'Filtrar': 'magnify'
+	}
+
+
+
+	def callback(self, instance):
+		print(instance.icon)
+		if instance.icon == 'text-box-search':
+			self.root.Listar()
+
+		elif instance.icon == 'notebook-plus-outline':
+			self.root.Adicionar()
+
+		if instance.icon == 'delete':
+			self.root.Remover()
+
+		if instance.icon == 'magnify':
+			self.root.Filtrar()
 	def build(self):
-		self.theme_cls.material_style = "M3"
+		
 		return VendasWindow()
+
+	
+
+
 
 	
 
