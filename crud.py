121cd1e9bce62cf0dfa28143ec1d0cc89012242c
jesiel364 <jesiel364@gmail.com>
2022-04-07 04:13:25 -0400

    On _start function

--- crud.py
+++ crud.py
@@ -11,7 +11,7 @@ from kivy.storage.jsonstore import JsonStore
 store = JsonStore('banco.json')
 
 # store.put('tito', nome = 'Mathieu', idade=32)
-Window.size = 300, 600
+#Window.size = 300, 600
 kv = Builder.load_file('crud.kv')
 
 
@@ -81,8 +81,8 @@ class MyCrud(MDApp):
 	'Adicionar': 'notebook-plus-outline', 
 	'Remover': 'delete', 'Filtrar': 'magnify'
 	}
-
-
+	def on_start(self):
+		self.root.Listar()
 
 	def callback(self, instance):
 		print(instance.icon)
