c44474a21b9516c455c5cc2ee9652b18d7c20ff9
jesiel364 <jesiel364@gmail.com>
2022-04-14 00:38:50 -0400

    Adicionado função de pesquisa, RecycleView e outras funcionalidades

--- crud.py
+++ crud.py
@@ -11,24 +11,34 @@ from kivymd.uix.snackbar import Snackbar
 from kivymd.uix.list import OneLineAvatarIconListItem
 from kivy.lang import Builder
 from kivy.core.window import Window
-from kivy.storage.jsonstore import JsonStore
+
+from kivy.uix.textinput import TextInput
+from kivymd.uix.label import MDLabel
+import re
+from kivy.uix.recycleview import RecycleView
+
+
 
 # Configurações de Tela
 from kivy.utils import platform
 if platform != 'android':
     Window.size = 300, 600
 
+from kivy.storage.jsonstore import JsonStore
 # Banco de Dados
-store = JsonStore('banco.json')
+store = JsonStore('database/banco.json')
 # store.put('tito', nome = 'Mathieu', idade=32)
 
 
 kv = Builder.load_file('crud.kv')
 
 
+		
+	
+
 class Item(OneLineAvatarIconListItem):
 	left_icon = StringProperty()
-	right_text = StringProperty('')
+	right_text = StringProperty()
 
 
 class ListComAvatar(TwoLineAvatarListItem):
@@ -37,7 +47,36 @@ class ListComAvatar(TwoLineAvatarListItem):
 
 class VendasWindow(MDBoxLayout):
 	def __init__(self, **kwargs):
-		super().__init__(**kwargs)		
+		super().__init__(**kwargs)
+
+	def existe(self, text='', search=False):
+		
+		def add_nome(nome, idade):
+			self.ids.rv.data.append(
+				{
+					'viewclass': 'ListComAvatar',
+					'text': str(nome),
+					'secondary_text': str(idade)
+					
+
+				}
+			)
+
+
+		self.ids.rv.data = []
+		
+		for key in store:
+			
+			nome = store.get(key)['nome']
+			idade = store.get(key)['idade']
+			if search:
+				if text in nome:
+					add_nome(nome, idade)
+			else:
+				add_nome(nome, idade)
+		
+		
+				
 
 	def Adicionar(self):
 		
@@ -69,49 +108,39 @@ class VendasWindow(MDBoxLayout):
 		toast('Test Kivy Toast')
 
 
-	def Listar(self):
-		res = store.find()
-		for key in store:
-			nome = store.get(key)['nome']
-			idade = store.get(key)['idade']
-
-			self.ids.container.add_widget(ListComAvatar(text = nome , secondary_text = f'{idade} anos'))
-	
-		for key in store:
-			print(store.get(key)['nome'])
-			print(store.get(key)['idade'])
-
-
 class MyCrud(MDApp):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.screen = VendasWindow()
 	data = {
 		'Mostrar todos': 'text-box-search',
 		'Adicionar': 'notebook-plus-outline', 
 		'Remover': 'delete', 'Filtrar': 'magnify'
 		}
-	def __init__(self, **kwargs):
-		super().__init__(**kwargs)
 		
 
 	def on_start(self):
-		self.root.Listar()
+		self.screen.existe()
 		
 		
 	def abrir(self, instance):
 		toast(instance.text)
 
+	
+
 	def callback(self, instance):
-		print(instance.icon)
-		if instance.icon == 'text-box-search':
-			self.root.Listar()
+		Snackbar(text = instance.icon).open()
+		# if instance.icon == 'text-box-search':
+		# 	self.root.Listar()
 
-		elif instance.icon == 'notebook-plus-outline':
-			self.root.Adicionar()
+		# elif instance.icon == 'notebook-plus-outline':
+		# 	self.root.Adicionar()
 
-		if instance.icon == 'delete':
-			self.root.Remover()
+		# if instance.icon == 'delete':
+		# 	self.root.Remover()
 
-		if instance.icon == 'magnify':
-			self.root.Filtrar()
+		# if instance.icon == 'magnify':
+		# 	self.root.Filtrar()
 	def build(self):
 		self.theme_cls.theme_style = "Light"
 		self.theme_cls.primary_palette = "Purple"
@@ -122,7 +151,7 @@ class MyCrud(MDApp):
 			items=menu_items, 
 			width_mult=4)
 	
-		return VendasWindow()
+		return self.screen
 
 	def chamada(self, button):
 		self.menu.caller = button
